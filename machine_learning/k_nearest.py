78528fdb480604ef18e197522d6e2170bdfe66b0
nakamasa <s1620660@u.tsukuba.ac.jp>
2016-10-16 16:38:20 +0900

    input spectrum

--- machine_learning/k_nearest.py
+++ machine_learning/k_nearest.py
@@ -4,70 +4,128 @@ import os
 from matplotlib.colors import ListedColormap
 from sklearn import neighbors, datasets, metrics
 
+#f = open('C:\\Users\\NakagawaMasafumi\\Documents\\data\\std.csv', 'r')
+#std=[[],[]]
+"""line_n = 0
+for line in f:
+	line_n += 1
+	if line_n == 3 or line_n == 7 or line_n == 11:
+		values = line.strip().split(',')
+		for i in range(0,4):
+			values[i] = float(values[i])
+			std[0].append(values[i])
+
+	if line_n == 4 or line_n == 8 or line_n == 12:
+		values = line.strip().split(',')
+		for i in range(0,4):
+			values[i] = float(values[i])
+			std[1].append(values[i])"""
 #dir_name = ['Walk', 'Climb_stairs']
-dir_name = ['nothing', 'open']
+#dir_name = ['nothing', 'open','dondon']
+#dir_name = ['dondon', 'open']
+dir_name = ['open']
 #index = 0
 inputs = []
 label = []
 for class_name in dir_name:
 	class_label = 0
-	if class_name=='open':
+	if class_name=='nothing':
+		class_label = 1
+	elif class_name=='dondon':
 		class_label = 1
 	#dir_path = 'C:\\Users\\NakagawaMasafumi\\Documents\\HMP_Dataset\\'+class_name+'_stats\\'
-	dir_path = 'C:\\Users\\NakagawaMasafumi\\Documents\\data\\'+class_name+'_stats\\'
+	dir_path = 'C:\\Users\\NakagawaMasafumi\\Documents\\data\\'+class_name+'_fft\\'
+	#dir_path = 'C:\\Users\\NakagawaMasafumi\\Documents\\data\\'+class_name+'_stats\\'
 	files = os.listdir(dir_path)
 
 	for file in files:
 		f = open(dir_path+file, 'r')
-		i=0
+		#i=0
+		index=0
 		input = []
+		row = 1
 		for line in f:
-			values = line.strip().split(',')
-			for j in range(0, 4):
-				input.append(float(values[j]))
-				i += 1
+			if row % 2 == 1:
+				values = line.strip().split(',')
+				values = list(map(float, values))
+				for i in range(0, 5):
+					input.append(sum(values[i*200:(i+1)*200]))
+
+				for i in range(0, 3):
+					input.append(sum(values[1000+i*500:1000+(i+1)*500]))
+
+				#print(len(input))
+			"""for j in range(0, 4):
+				#if j==3:
+				#	input.append((float(values[j])-std[0][i])/std[1][i])
+				#else:
+				input.append((float(values[j])-std[0][i])/std[1][i])
+				i += 1"""
+			row += 1
 
 		inputs.append(input)
 		label.append(class_label)
 
+#print(inputs)
 inputs = np.array(inputs)
 label = np.array(label)
 indices = np.random.permutation(len(inputs))
-inputs_train = inputs[indices[:-64]]
-label_train = label[indices[:-64]]
-inputs_test = inputs[indices[-64:]]
-label_test = label[indices[-64:]]
+inputs_train = inputs[indices[:-40]]
+label_train = label[indices[:-40]]
+inputs_test = inputs[indices[-40:]]
+label_test = label[indices[-40:]]
+"""inputs_test = []
+label_test = []
+
+dir_path = 'C:\\Users\\NakagawaMasafumi\\Documents\\data\\dondon_fft\\'
+files = os.listdir(dir_path)
+
+for file in files:
+	#print (file)
+	f = open(dir_path+file, 'r')
+	i=0
+	input_test = []
+	row = 1
+	for line in f:
+		if row % 2 == 1:
+			values = line.strip().split(',')
+			values = list(map(float, values))
+			for i in range(0, 5):
+				input_test.append(sum(values[i*200:(i+1)*200]))
+
+			for i in range(0, 3):
+				input_test.append(sum(values[1000+i*500:1000+(i+1)*500]))
+		row += 1
+	for line in f:
+		values = line.strip().split(',')
+		for j in range(0, 4):
+			input_test.append((float(values[j])-std[0][i])/std[1][i])
+			#input_test.append(float(values[j])/500)
+			i += 1
+	inputs_test.append(input_test)
+	label_test.append(2)"""
+#np.append(label_test,2)
+#print (len(inputs_test))
 #print(inputs)
 #print(label)
-#iris = datasets.load_iris() # 4次元、150サンプルのデータセットで
-                            # 植物の萼（がく）の長さ、幅、 花びらの長さ、幅、単位はcm。
-#iris_X = iris.data[:, :2]  # 4次元のうち、最初の2次元の特徴量だけを使用
-#iris_y = iris.target # 正解ラベル, 0, 1, 2の3種類
-#print(iris_X)
-#print(iris_y)
 
 #########################################
 np.random.seed(0) # 乱数のシード設定、0じゃなくてもなんでもいい
-#indices = np.random.permutation(len(iris_X)) # 0~149の数字をランダムに並び替え
-# 140個のデータセットを105個の教師データと35個のテストデータに分ける
-#iris_X_train = iris_X[indices[:-35]]
-#iris_y_train = iris_y[indices[:-35]]
-#iris_X_test = iris_X[indices[-35:]]
-#iris_y_test = iris_y[indices[-35:]]
 
-#########################################
 cmap_light = ListedColormap(['#FFAAAA', '#AAFFAA', '#AAAAFF'])
 cmap_bold = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 #########################################
 h = 0.1 # メッシュサイズ
-k_list = [1, 3, 5, 10] # k の数
+k_list = [1,3] # k の数
 weights_list =['uniform', 'distance']
+#weights_list =['distance']
 score = np.zeros((len(k_list)*2,5)) # score
 
 #########################################
 #plt.figure(figsize=(8*len(k_list), 12))
-threshold = 450
+#threshold = 0.005
+threshold = 90000
 i = 1 # subplot用
 for weights in weights_list:
 	for k in k_list:
@@ -89,24 +147,26 @@ for weights in weights_list:
 		#plt.title("k = %i, weights = '%s'" % (k, weights), fontsize=30)
 		#print(clf.predict_proba(inputs_test))
 		distset = clf.kneighbors(inputs_test,k)[0]
-		#print(distset)
+		#print(clf.kneighbors(inputs_test[0],k))
 		correct = 0
 		incorrect = 0
 		data_index = 0
 		for dists in distset:
 			data_index += 1
 			for d in dists:
+				#print(d)
 				if d > threshold:
-					print('disorder at line:'+str(data_index))
+					#print('disorder at line:'+str(data_index))
 					print(str(d))
 					if label_test[data_index-1] > 1:
 						#異常検知の正解数不正解数を記録
 						correct += 1
 					else:
 						incorrect += 1
+					break
 
 		print(str(correct)+'/'+str(incorrect))
-		score[i-1,3] = k
+		"""score[i-1,3] = k
 		score[i-1,0] = metrics.f1_score(label_test, clf.predict(inputs_test),average='weighted')
 		score[i-1,1] = metrics.precision_score(label_test, clf.predict(inputs_test))
 		score[i-1,2] = metrics.recall_score(label_test,clf.predict(inputs_test))
@@ -128,4 +188,4 @@ for weights in weights_list:
     plt.xlabel("k_neighbors")
     plt.ylabel("f1, prec, recall")
     i = i + 1
-plt.show()
+plt.show()"""
