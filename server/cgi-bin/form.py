93cbe9c660edc20634362192ca849835a6723e42
otomoyoshi <tomoyoshi.o.ib@gmail.com>
2016-11-08 21:17:53 +0900

    POSTを利用したHTTP通信のファイル送信・受信プログラム

--- server/cgi-bin/form.py
+++ server/cgi-bin/form.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
+print('Content-type: text/html; charset=UTF-8 \r\n')
 
 html='''
 <!DOCTYPE html>
 <html><body>
-name: %s
-mail:%s
+<!--name: %s-->
 </html></body>
 '''
 
@@ -18,15 +18,64 @@ ERROR!
 
 import cgi
 form = cgi.FieldStorage()
+import cgitb
+cgitb.enable()
 
 print("Content-Type: text/html\n")
-#if not getvalue('name','not found') and not getvalue('name', 'not found'):
-print(html % (form['name'].value, form['mail'].value))
+print('<br>')
 
-# ファイル書き込み処理
+print("form['file']")
+print(form['file'])
+print('<br>')
 
-file = open("result/test.txt",'w')
-file.write(form['name'].value + ','+ form['mail'].value)
-file.close()
-print('write')
+print("form['file'].value")
+print(form['file'].value)
+print('<br>')
+
+print("form['file'].name")
+print(form['file'].name)
+print('<br>')
+
+print("form['file'].filename")
+print(form['file'].filename)
+print('<br>')
+
+fileName = "result.txt"
+
+from struct import unpack
+if 'file' in form:
+	print('has_key')
+	print('<br>')
+
+	item = form['file']
+	print("item.file")
+	print(item.file)
+	print('<br>')
+
+	if item.file:
+		#print('before_open')
+		filePath = open(fileName, 'wb')		
+		#fileData = file.item.read()
+		fileData = form['file'].value
+		print("filePath")
+		print(filePath)
+		print('<br>')
+
+		print("filePath")
+		print(fileData)
+		print('<br>') 
+
+		filePath.write(fileData)
+		#print('finish')
+		filePath.close()
+
+print('<br>')
+print("getFileName:[")
+print(fileName)
+print("]")
+print("-->")
+print("saveFileName:[")
+print(form['file'].filename)
+print("]")
+
 
