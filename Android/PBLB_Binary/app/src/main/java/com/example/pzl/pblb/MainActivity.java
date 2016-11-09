3a909c87e3965365f7de7f7c2179f39f8ef10948
peter <hansinrai@gmail.com>
2016-11-09 15:22:48 +0900

    binary&string

---
+++ Android/PBLB_Binary/app/src/main/java/com/example/pzl/pblb/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.pzl.pblb;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import jp.ksksue.driver.serial.FTDriver;
+
+
+public class MainActivity extends Activity {
+    boolean flag = false;
+    FTDriver mSerial;
+    final int mOutputType = 0;
+    int i = 0;
+    Handler mHandler = new Handler();
+    final int SERIAL_BAUDRATE = FTDriver.BAUD115200;
+    private boolean mStop = false;
+    private static final String ACTION_USB_PERMISSION =
+            "jp.ksksue.tutorial.USB_PERMISSION";
+    private boolean mRunningMainLoop;
+    Button start, stop;
+    private LinearLayout mainLayout;
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // save the current data, for instance when changing screen orientation
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedState) {
+        super.onRestoreInstanceState(savedState);
+        // restore the current data, for instance when changing the screen
+        // orientation
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // タイトルを非表示にします。
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        //currentTime = 0;
+        mSerial = new FTDriver((UsbManager) getSystemService(Context.USB_SERVICE));
+
+        // [FTDriver] setPermissionIntent() before begin()
+        PendingIntent permissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(
+                ACTION_USB_PERMISSION), 0);
+        mSerial.setPermissionIntent(permissionIntent);
+
+
+        start = (Button) findViewById(R.id.button1);
+        stop = (Button) findViewById(R.id.button2);
+
+
+        //測定開始ボタンの処理
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mStop = false;
+                if (mSerial.begin(SERIAL_BAUDRATE)) {
+                    mainloop();
+                    start.setEnabled(false);
+                    stop.setEnabled(true);
+                }
+            }
+        });
+
+        //測定開始ボタンの処理
+        stop.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mStop = true;
+                stop.setEnabled(false);
+                start.setEnabled(true);
+            }
+        });
+    }
+
+    private void mainloop() {
+        mRunningMainLoop = true;
+        new Thread(mLoop).start();
+    }
+
+    private Runnable mLoop = new Runnable() {
+        @Override
+        public void run() {
+            int len = 0;
+            while (true) {
+                // [FTDriver] Create Read Buffer
+                byte[] rbuf = new byte[256];
+                len = mSerial.read(rbuf);
+                if (len != -1) {
+                    int youso = rbuf.length;
+                    if (flag == false) {
+                        if (youso > 30) {
+                            flag = true;
+                            try {
+                                saveToSDCard("pbl_orange.csv", rbuf);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                            mHandler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    flag = false;
+                                }
+                            }, 5000);
+                        }else{
+                            try {
+                                saveToSDCard1("pbl_orange.csv", "生存信号");
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    } else {
+                        try {
+                            saveToSDCard2("pbl_orange.csv", rbuf);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    if (mStop) {
+                        mRunningMainLoop = false;
+                        return;
+
+                    }
+                }
+            }
+        }
+    };
+
+
+    public void saveToSDCard(String filename,byte[] ss) throws Exception{
+        File file=new File(Environment.getExternalStorageDirectory(), filename);
+        FileOutputStream out=new FileOutputStream(file,false);
+        out.write(ss);
+        out.close();
+    }
+
+    public void saveToSDCard2(String filename,byte[] ss) throws Exception{
+        File file=new File(Environment.getExternalStorageDirectory(), filename);
+        FileOutputStream out=new FileOutputStream(file,true);
+        out.write(ss);
+        out.close();
+    }
+
+    public void saveToSDCard1(String filename,String ss) throws Exception{
+        File file=new File(Environment.getExternalStorageDirectory(), filename);
+        FileOutputStream out=new FileOutputStream(file,false);
+        out.write(ss.getBytes());
+        out.close();
+    }
+
+
+}
