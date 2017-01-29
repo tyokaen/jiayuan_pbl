5a4835dddbd4b69eac4e7ba4052544faf5f66fbb
nakamasa <s1620660@u.tsukuba.ac.jp>
2017-01-29 11:03:37 +0900

    revised the constitution of directory

--- twe-lite/Samp_I2C2/Main/Version.mk
+++
@@ -1,5 +0,0 @@
-VERSION_MAIN = 0
-VERSION_SUB  = 2
-VERSION_VAR  = 0
-
-# v0.2.0 初版
---
+++ twe-lite/micom/Main.h
@@ -0,0 +1,14 @@
+#ifndef  SLAVE_H_INCLUDED
+#define  SLAVE_H_INCLUDED
+
+#if defined __cplusplus
+extern "C" {
+#endif
+
+#include "config.h"
+
+#if defined __cplusplus
+}
+#endif
+
+#endif  /* SLAVE_H_INCLUDED */
