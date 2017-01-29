01d8b10d9dc1679d947443d0efbfbcd7168ba7c7
otomoyoshi <tomoyoshi.o.ib@gmail.com>
2017-01-29 16:31:35 +0900

    リリース3プログラム

--- server/cgi-bin/form3.py
+++ server/cgi-bin/form3.py
@@ -19,12 +19,12 @@ import pywt
 sys.stdout.write('Content-type: text/html; charset=UTF-8\n')
 
 #print(os.getcwd())
-#save_dir = ['result']
-#cwd = os.getcwd() +'\\'+ save_dir[0] + '\\'
-#print(cwd)
+save_dir = ['result','model']
+cd = os.getcwd() +'/'+ save_dir[0] + '/'
+#print(cd)
 
 
-cd = "/home/orange/workspace/result/"
+#cd = "/home/orange/workspace/result/"
 
 if not 'jiayuan' in form:
     sys.exit("failure-jiayuan")
@@ -39,10 +39,7 @@ if 'jiayuan' in form:
     filePath.write(fileData)
     filePath.close()
 
-#    dirName = "/home/orange/workspace/result/"
- 
-#    for file in files:
-#    file = getName
+
     f = open(getName, 'rb')
     acc_list = [[], [], []]
 
@@ -56,12 +53,13 @@ if 'jiayuan' in form:
                 acc_list[i%3].append(signal[i])
     f.close()
 
-    ACC_THRESHOLD = 900
+    ACC_THRESHOLD = 800
 
     for i in acc_list:
         #sys.exit(str(len(i)))
         if len(i) <= ACC_THRESHOLD:
-            sys.exit("acc_list <= " + str(ACC_THRESHOLD))
+            #sys.exit(strlen(i))
+            sys.exit("acc_list"+ str(len(i)) +"<=" + str(ACC_THRESHOLD))
 
 
     dec = 0
@@ -98,7 +96,10 @@ if 'jiayuan' in form:
     
     inputs = np.array(inputs)
     
-    clfPath = "/home/orange/workspace/model/knn_clf"
+   #clfPath = "/home/orange/workspace/model/knn_clf"
+    clfPath = os.getcwd() +'/' + save_dir[1] + '/knn_clf'
+    #sys.exit(clfPath)
+
     
     if os.path.isfile(clfPath):
         clf = joblib.load(clfPath)
@@ -112,7 +113,8 @@ if 'jiayuan' in form:
 
     
     #threshold = 185 # model_10    
-    threshold = 110 # 12_18_v2 model_10
+    #threshold = 110 # 12_18_v2 model_10
+    threshold = 400 # 1_17
     status = 'normal'
     dist=0
     for dists in distset:
@@ -142,13 +144,55 @@ if 'jiayuan' in form:
     req = urllib.request.Request("https://android.googleapis.com/gcm/send")
     req.add_header("Authorization", "key=AIzaSyCO3NJALjY_0HAUHA_yIn7Hhe4QFRTvFY4")
     req.add_header("Content-Type", "application/json")
+
+    import json
+    import urllib.request
+    
+    APPNO = 'e3538b500de44713bbe6f33a5279f185'
+    APIKEY = 'baa0c2564c1c4506b2a5f46120f72b69'
+    endpoint = 'https://api.push7.jp/api/v1/{}/send'.format(APPNO)   
+    header_send = {'Content-Type':'application/json'}
+    method = 'POST'
+    '''
+    move_dirPath = "/root/workspace/movie/setting.txt"
+    movefile = open(move_dirPath, "r")
+    moveName = ""
+    for filename in movefile:
+        moveName = filename
+
+    movePath = "http://215.129.63.215:8080/movie/" + moveName
+    movefile.close()
+    '''
+    #sys.exit(movePath)
+    
     if status == 'warning':
-        data = {"registration_ids":["APA91bE5eguxZPbeXa12yZt0cHfa3yZJJ5MAqIiNTPl5VWgcrn_D55CoeCVd49OEDY7oEmZ3xHickGvD53_ZOvfg10KChSedHupsl7j38MZaJkMST9mWi5r3CVzTHQ5OlpUCGDBNF6kD"], "data":{"message":str(file_name) + 'warning'}}
+        data = {"registration_ids":["APA91bEMKY97PyQjmpJFNZe3WWQDOt3OnTuzWPwl9N58wh1wK7gAFN8msXtcoa0crbyF9kF48oPKo9aag3IAWsZipqfDl78XmcahcW4TFwJSobeKkgXclOfSLmBfhL4xDxd5Dryz0Y3j"], "data":{"message":str(file_name) + 'warning'}}
+
+        payload = {
+                "apikey":APIKEY,
+                "url":"http://210.129.63.215:8080/cgi-bin/hello.py",
+                "icon":"http://210.129.63.215:8080/cgi-bin/orange_icon.jpg",
+                "body":"warning",
+                "title":"orange"
+                }      
+        json_data = json.dumps(payload,indent=4).encode("utf-8")
+        request = urllib.request.Request(endpoint,data=json_data, method=method,headers=header_send)
+
         sys.stdout.write(html % 'warning')
         #sys.exit("warning!")
     else:
-        data = {"registration_ids":["APA91bE5eguxZPbeXa12yZt0cHfa3yZJJ5MAqIiNTPl5VWgcrn_D55CoeCVd49OEDY7oEmZ3xHickGvD53_ZOvfg10KChSedHupsl7j38MZaJkMST9mWi5r3CVzTHQ5OlpUCGDBNF6kD"], "data":{"message":str(file_name) + 'door-open'}}
-        sys.stdout.write(html %'door-open')
+        data = {"registration_ids":["APA91bEMKY97PyQjmpJFNZe3WWQDOt3OnTuzWPwl9N58wh1wK7gAFN8msXtcoa0crbyF9kF48oPKo9aag3IAWsZipqfDl78XmcahcW4TFwJSobeKkgXclOfSLmBfhL4xDxd5Dryz0Y3j"], "data":{"message":str(file_name) + 'door-open'}}
+        sys.stdout.write(html % 'door-open')
+        payload = {
+                "apikey":APIKEY,
+                "url":"http://210.129.63.215:8080/cgi-bin/form6.py",
+                "icon":"http://210.129.63.215:8080/cgi-bin/orange_icon.jpg",
+                "body":"door-open",
+                "title":"orange"
+                }    
+        json_data = json.dumps(payload,indent=4).encode("utf-8")
+        request = urllib.request.Request(endpoint,data=json_data, method=method,headers=header_send)
+
         #sys.exit("door-open")
      
     import json
@@ -156,3 +200,9 @@ if 'jiayuan' in form:
     with urllib.request.urlopen(req, json_data) as res:
         html = res.read().decode("utf-8")
         #sys.stdout.write(html)
+
+    with urllib.request.urlopen(request) as response:
+            response_body = response.read().decode("utf-8")
+            #print("response_body:{}".format(response_body))
+
+        
