ea9357c15ffcbf1549df031db08546da6ec1a565
otomoyoshi <tomoyoshi.o.ib@gmail.com>
2017-02-10 04:13:23 +0900

    多クラス分類SVMコメントあり

--- machine_learning/interaction_svm2.py
+++ machine_learning/interaction_svm2.py
@@ -10,6 +10,7 @@ Created on Wed Jan 25 03:48:54 2017
 
 import os
 import numpy as np
+import pandas as pd
 from sklearn import cross_validation
 import time
 from sklearn import svm
@@ -18,6 +19,18 @@ import pickle
 import random
 import shutil
 from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import KernelPCA
+from sklearn.svm import SVC
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+
+
+#--------------------------------------------------------------------------------------------#
+                                    # 振動データの保存先を指定--start
+#--------------------------------------------------------------------------------------------#
 
 #dir_name = ['clatter_12_18_csv_fft','open_12_18_csv_fft']
 #dir_name = ['open_12_18_csv_beforelinear_fft','clatter_12_18_csv_beforelinear_fft']
@@ -27,18 +40,31 @@ from sklearn.multiclass import OneVsRestClassifier
 #dir_name = ['sku445-753_finger_afterlinear_fft','sku469-657_finger_afterlinear_fft']
 #dir_name = ['sku445-753_afterlinear_fft','sku469-657_afterlinear_fft', 'sku186-580_afterlinear_fft']
 #dir_name = ['sku445-753_beforelinear_fft','sku469-657_beforelinear_fft', 'sku186-580_beforelinear_fft']
-dir_name = ['ballpen_afterlinear','marker_afterlinear','sharppen_afterlinear','sku445-753_afterlinear_fft','sku469-657_afterlinear_fft', 'sku186-580_afterlinear_fft']
+#dir_name = ['ballpen_afterlinear','marker_afterlinear','sharppen_afterlinear','sku445-753_afterlinear_fft','sku469-657_afterlinear_fft', 'sku186-580_afterlinear_fft']
+#dir_name = ['sku445-753_afterlinear_fft','sku469-657_afterlinear_fft', 'sku186-580_afterlinear_fft','t480-4888_afterlinear_fft']#01
+#dir_name = ['sku445-753_afterlinear_fft','sku469-657_afterlinear_fft', 'sku186-580_afterlinear_fft']#02
+#dir_name = ['pen_csv_afterlinear2_fft2','tile_csv_afterlinear2_fft2'] #2class
+#dir_name = ['ballpen','enpitsu','marker','sharppen']
+dir_name = ['sku186-580','sku445-753','sku469-657','t480-4888']
+#dir_name = ['ballpen','enpitsu','marker','sharppen','sku186-580','sku445-753','sku469-657','t480-4888']
+#--------------------------------------------------------------------------------------------#
+                                    # 振動データの保存先を指定--end
+#--------------------------------------------------------------------------------------------#
 
-
-
+#dir_p = ['01haptics']
+#dir_p = ['pen_csv_afterlinear2_fft2']
+#dir_p = ['tile_csv_afterlinear2_fft2']
+dir_p = ['pen_tile']
 
 datas = []
 files = []
 for dirname in dir_name:
+    # 振動データ保存先ディレクトリ名を指定
 #    dir_path = 'C:\\Users\\student\\Documents\\data\\'+dirname+'\\'
 #    dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv\\'+dirname+'\\'
 #    dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_finger_csv\\'+dirname+'\\'
-    dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dirname+'\\'
+    dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name + '\\'
+#    dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_p[0]+'\\'+dirname+'\\'
 
     datas.append(np.array(os.listdir(dir_path)))
 #    files.extend(os.listdir(dir_path))
@@ -48,22 +74,24 @@ for dirname in dir_name:
 dir_i = 0
 save_dir = '_'.join(dir_name[dir_i].split('_')[1:5])
 
-#dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[dir_i] + '\\'
-#out_dir_path = 'C:\\Users\\student\\Documents\\data\\' + save_dir + '_svm\\'
+# 出力先ディレクトリ名を指定
+out_dir_path = 'C:\\Users\\student\\Documents\\data\\' + save_dir + '_svm\\'
 #out_dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv' + save_dir + '_svm\\'
 #out_dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_finger_csv' + save_dir + '_svm\\'
-out_dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\' + save_dir + '_svm\\'
+#out_dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_p[0]+'\\' + save_dir + '_svm\\'
 
 
 
+# 出力先のディレクトリが存在しない場合生成
+# 出力先のディレクトリが存在する場合削除して、初期化する
 if os.path.isdir(out_dir_path) is False:
     os.makedirs(out_dir_path)
     print("make dir")
-#else:
-#    shutil.rmtree(out_dir_path)
-#    print("delete dir")
-#    os.makedirs(out_dir_path)
-#    print("make dir")
+else:
+    shutil.rmtree(out_dir_path)
+    print("delete dir")
+    os.makedirs(out_dir_path)
+    print("make dir")
 
 
 
@@ -76,63 +104,11 @@ for index,files in enumerate(datas):
         label = []
         input = []
         row = 1
-#    
-#        if index == 0:
-#           dir_i = 0
-##           dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[dir_i]+'\\'
-##           dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv\\'+dir_name[dir_i]+'\\'
-#           dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_finger_csv\\'+dir_name[dir_i]+'\\'
-#
-#        else:
-#            dir_i = 1
-##            dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[dir_i]+'\\' 
-##            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv\\'+dir_name[dir_i]+'\\'
-#            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_finger_csv\\'+dir_name[dir_i]+'\\'
-#            
-    
-#        if index == 0:
-#           dir_i = 0
-##           dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[dir_i]+'\\'
-##           dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv\\'+dir_name[dir_i]+'\\'
-#           dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dir_name[dir_i]+'\\'
-#
-#        elif index == 1:
-#            dir_i = 1
-##            dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[dir_i]+'\\' 
-##            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv\\'+dir_name[dir_i]+'\\'
-#            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dir_name[dir_i]+'\\'
-#        else:
-#            dir_i = 2
-#            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dir_name[dir_i]+'\\'
-
-        if index == 0:
-           dir_i = 0
-#           dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[dir_i]+'\\'
-#           dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv\\'+dir_name[dir_i]+'\\'
-           dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dir_name[dir_i]+'\\'
-
-        elif index == 1:
-            dir_i = 1
-#            dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[dir_i]+'\\' 
-#            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv\\'+dir_name[dir_i]+'\\'
-            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dir_name[dir_i]+'\\'
-        elif index == 2:
-            dir_i = 2
-            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dir_name[dir_i]+'\\'  
-        elif index == 3:   
-           dir_i = 3
-#           dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[dir_i]+'\\'
-#           dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv\\'+dir_name[dir_i]+'\\'
-           dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dir_name[dir_i]+'\\'
-
-        elif index == 4:
-            dir_i = 4
-#            dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[dir_i]+'\\' 
-#            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics_csv\\'+dir_name[dir_i]+'\\'
-            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dir_name[dir_i]+'\\'
-        else:
-            dir_i = 5
-            dir_path = 'C:\\Users\\student\\Documents\\data\\haptics3_csv\\'+dir_name[dir_i]+'\\'        
+  
+        
+#        dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_p[0]+'\\'+dir_name[index]+'\\'
+        dir_path = 'C:\\Users\\student\\Documents\\data\\'+dir_name[index]+'\\'
+     
 
     #    print(str(file))
         file = open(dir_path+file, 'r')
@@ -142,38 +118,16 @@ for index,files in enumerate(datas):
 #            print("len(value):{}".format(len(values)))
 #            print("file:{}".format(file))
 
+            # フーリエ変換した値を50ずつ合計する
+            SUM = 50
             values = list(map(float, values))
-            devi = int(len(values) / 50)
-            for i in range(0, 50):
+            devi = int(len(values) / SUM)
+            for i in range(0, SUM):
                 input.append(sum(values[i*devi:(i+1)*devi]))
 #            row += 1
-#        if index == 0:
-#            label.append(0)
-#        else:
-#            label.append(1)
-            
-#        if index == 0:
-#            label.append(0)
-#        elif index == 1:
-#            label.append(1)
-#        else:
-#            label.append(2)
 
-        if index == 0:
-            label.append(0)
-        elif index == 1:
-            label.append(1)
-        elif index == 2:
-            label.append(2)
-        elif index == 3:
-            label.append(3)
-        elif index == 4:
-            label.append(4)
-        else:
-            label.append(5)
-            
-            
-                
+        label.append(index)
+                 
         features.append(input)
         labels.append(label)
         file.close()
@@ -182,45 +136,55 @@ features = np.array(features)
 #labels = np.array(label)
 labels = np.array(labels)
 random = random.randint(1,10)
+
+# 交差検証
 training_datas, test_datas, training_labels, test_labels = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=random)
 
-#fix = 100
-#training_datas, test_datas, training_labels, test_labels = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=fix)
-
-
-################################################################################
-#                                   SVM
-################################################################################
 
 
 
-start = time.time()
+start = time.time() #計測開始
+#------------------------------------------------------------------------------------------#
+                                    # SVMの種類を選ぶ　--start
+#------------------------------------------------------------------------------------------#
+
+
 #svm = svm.SVC(kernel='linear',class_weight="balanced",decision_function_shape="ovo",random_state=0)
 #svm = svm.SVC(kernel='linear',class_weight="balanced",decision_function_shape="ovr",random_state=0)
-#svm = svm.SVC(kernel='rbf', C=10.0 ,degree=3, random_state=0)
+#svm = svm.SVC(kernel='rbf', C=1.0 ,degree=3, random_state=0)
 #svm = svm.SVC(kernel='poly', C=1.0 ,degree=3, random_state=0)
 #svm = svm.SVC(kernel='poly', C=1.0 ,degree=4, random_state=0)
 svm = svm.SVC(kernel='poly')
+#svm = svm.SVC(kernel='rbf')
 #print(svm)
 
 #svm = OneVsRestClassifier(svm) # コメントアウトするとone-against-one
 
+#------------------------------------------------------------------------------------------#
+                                    # SVMの種類を選ぶ　--end
+#------------------------------------------------------------------------------------------#
 
-#線形svmのモデルにトレーニングデータを適合させる
+#SVMの学習 
 svm_fit = svm.fit(training_datas, training_labels) 
+#svm_fit = svm.fit(training_datas_norm, training_labels) 
+
+
+
+# 学習したモデルを保存
+pickle.dump(svm_fit,open(out_dir_path+"pickle.dump","wb"))
+#print('学習モデル：%s' % svm_fit) 
+
+
+#　SVM予測
+pred = svm_fit.predict(test_datas)
+#pred = svm_fit.predict(test_datas_norm)
+
+# 計測終了
 processing_time = time.time() - start
 
-pickle.dump(svm_fit,open(out_dir_path+"pickle.dump","wb"))
-
-print('学習モデル：%s' % svm_fit) 
-
-#test_datas = test_datas[0:300]  
-#test_labels = test_datas[0:300] 
-#     2値分類予測
-pred = svm_fit.predict(test_datas)
-
-
-# 正例と負例を表示
+#------------------------------------------------------------------------------------------#
+                                    # 正例と負例を表示　--start
+#------------------------------------------------------------------------------------------#
 #TP = 0
 #TN = 0
 #FP = 0
@@ -245,39 +209,64 @@ pred = svm_fit.predict(test_datas)
 #            continue
 #print(count)
 #print("TN:{}/TP:{}/FP:{}/FN:{}".format(TN,TP,FN,FP))
+#------------------------------------------------------------------------------------------#
+                                    # 正例と負例を表示　--end
+#------------------------------------------------------------------------------------------#
+
 
 print('正解:%s' % test_labels)
 print('予測：%s' % pred)
 print('処理時間 : %f' % processing_time)
-
-
-#target_names=['0','1']
-#target_names=['0','1','2']
-target_names=['0','1','2','3','4','5']
-
-print('評価：%s' % classification_report(test_labels, pred, target_names=target_names))
+print('評価：%s' % classification_report(test_labels, pred, target_names=dir_name))
 #print('評価：%s' % classification_report(test_labels, pred))
 
 
 
-#save_name = ['svm_fft_one_against_rest_report.txt','svm_fft_one_against_one_report.txt']
-save_name = ['svm6_fft_one_against_rest_report.txt','svm6_fft_one_against_one_report.txt']
 
-save_i = 0
+#------------------------------------------------------------------------------------------#
+                        # 指定した名前でConfusion Matrixを保存する　--start
+#------------------------------------------------------------------------------------------#
+
+save_i = 1 # save_name[save_i]
+# save_name[0],save_name[1],save_name[2]
+save_name = ['2class_poly.txt','4class_poly_report.txt','8class_poly_report.txt']
+
+#------------------------------------------------------------------------------------------#
+                        # 指定した名前でConfusion Matrixを保存する　--end
+#------------------------------------------------------------------------------------------#
 
 
+# ファイル名（save_name[save_i]）を生成
 if os.path.isfile(out_dir_path + save_name[save_i]) is False:
     f = open(out_dir_path + save_name[save_i], 'w')
     f.write("make file")
     f.close()
     print("make file")
+    
 
 
+# cofusion matrixを生成
+cnf_matrix = confusion_matrix(pred,test_labels)
+    
+# 次の4つの内容を保存
+#　■　「分類している全ての振動データの保存先名」
+#　■　「svmパラメータ」
+#　■　「f値」
+#　■　「confusion matrix」
 f = open(out_dir_path + save_name[save_i], 'a')
 #f.write('{}と{}の分類（fft）：'.format(dir_name[0],dir_name[1]))
-f.write('{}と{}と{}の分類（fft）：'.format(dir_name[0],dir_name[1],dir_name[2]))
-
-f.write('{}'.format(classification_report(test_labels,pred, target_names=target_names)))
-f.write('処理時間:{}'.format(processing_time))
+f.write('{}の分類（fft_normal）：'.format(dir_name))
+f.write("\n")
+f.write('パラメータ{}*****'.format(svm))
+f.write("\n")
+f.write('{}'.format(classification_report(test_labels,pred, target_names=dir_name)))
+f.write("\n")
+f.write("{}".format(pd.DataFrame(cnf_matrix,index=dir_name,columns = dir_name)))
+f.write("\n")
+#f.write('処理時間:{}*****'.format(processing_time))
 f.close()
+
+
+
+
 
